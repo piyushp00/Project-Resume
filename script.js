8f196503d66ce52802e92579b3128d1619069903
Piyush Pandey <piyushpvns@gmail.com>
2020-10-11 11:53:02 +0530

    Smooth Scroll
    Added smooth scroll behavior to the pages

--- script.js
+++ script.js
@@ -1,39 +1,23 @@
-/*
-var targetPos = 1000;
 
-var currPos = 0;
 
-var scrollInterval = setInterval(function(){
-  if(currPos >= targetPos) {
-    clearInterval(scrollInterval);
-    return;
-  }
-  
-  currPos += 50;
-  window.scrollBy(0, 50);
-}, 50);
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+console.log(navMenuAnchorTags);
 
-*/
-
-var ed = document.getElementById('elink');
-var eduSection = getElementById('education');
-ed.addEventListener('click', function(event){
+for (var i = 0; i < navMenuAnchorTags.length; i++) {
+  navMenuAnchorTags[i].addEventListener('click', function (event) {
     event.preventDefault();
-    
-    var coordinates = eduSection.getBoundingClientRect();
-    var tPos = coordinates.y;
-    var currPos = 0;
 
-    var scrollInterval = setInterval(function(){
-        if(currPos >= tpos) {
-          clearInterval(scrollInterval);
-          return;
-        }
-        
-        currPos += 50;
-        window.scrollBy(0, 50);
-      }, 50);
+    var targetSectionID = this.textContent.trim().toLowerCase();
+    var targetSection = document.getElementById(targetSectionID);
+    console.log(targetSection);
 
-});
-
-
+    var interval = setInterval(function () {
+      var targetSectionCoordinates = targetSection.getBoundingClientRect();
+      if (targetSectionCoordinates.top <= 0) {
+        clearInterval(interval)
+        return;
+      }
+      window.scrollBy(0, 50);
+    }, 15);
+  });
+}
